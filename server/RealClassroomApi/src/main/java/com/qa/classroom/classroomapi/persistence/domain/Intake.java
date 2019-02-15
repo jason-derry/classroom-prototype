d2d218366c2a301fed91e9b1ddde35f6cf0bd5ed
jasond-qa <jason.derry@academytrainee.com>
2019-02-15 10:00:26 +0000

    added attributes to intake class

--- server/RealClassroomApi/src/main/java/com/qa/classroom/classroomapi/persistence/domain/Intake.java
+++ server/RealClassroomApi/src/main/java/com/qa/classroom/classroomapi/persistence/domain/Intake.java
@@ -1,6 +1,22 @@
 package com.qa.classroom.classroomapi.persistence.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Entity
 public class Intake {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public long intakeId;
+	public int size;
+	public int classroomId;
+	
+	public Intake() {
+	}
+	
+	public Intake(
 
 }
